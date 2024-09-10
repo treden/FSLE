f270daf1a05228e7787ad22d3ae034bfe3f96409
Yann-Treden Tranchant <36726387+treden@users.noreply.github.com>
2024-09-10 14:57:41 +1000

    Update create_lagrangian_env.sh
    Update command line

--- create_lagrangian_env.sh
+++ create_lagrangian_env.sh
@@ -1,6 +1,6 @@
 conda create -y --name lagrangian  python=3.7.0
 source activate lagrangian
 
-conda install -y -c conda-forge -c anaconda -c fbriol python-dateutil netcdf4 dask libboost=1.71.0 lagrangian 
+conda install fbriol::lagrangian python-dateutil netcdf4 dask libboost=1.71.0
 conda install xarray matplotlib cartopy
 pip install notebook
