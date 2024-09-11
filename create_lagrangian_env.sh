998b82cc03375d57e83aa9959488b0148e2ae4ca
Yann-Treden Tranchant <yann-treden.tranchant@csiro.au>
2024-09-11 10:28:15 +1000

    fix errors (Maya meeting)

--- create_lagrangian_env.sh
+++ create_lagrangian_env.sh
@@ -1,6 +1,7 @@
 conda create -y --name lagrangian  python=3.7.0
 source activate lagrangian
 
-conda install fbriol::lagrangian python-dateutil netcdf4 dask libboost=1.71.0
+conda install fbriol::lagrangian  python-dateutil netcdf4=1.4.2 dask libboost=1.71.0 
 conda install xarray matplotlib cartopy
-pip install notebook
+
+### pip install notebook
